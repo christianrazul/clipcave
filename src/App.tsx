dbcbacd571644c40a4f56d7e8eb83e4747c72384
Rudolph Christian Razul <razulchristian@gmail.com>
2024-06-17 22:36:45 +0800

    Add ts types to fix ts compilation error

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import { Box } from "@mui/material";
-
 const App = () => {
-  return (
-    <Box className="bg-blue-200">
-      <h1 className="text-xl text-red-500">Test</h1>
-    </Box>
-  );
+  return <div>App</div>;
 };
 
 export default App;
